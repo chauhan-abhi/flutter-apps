7869f2245cd2c36a21f2597d70ed8b01f201391f
Abhijeet Singh Chauhan <chauhanabhi1245@gmail.com>
2019-11-17 19:17:06 +0530

    bottom nav bar created

--- instagramui_clone/lib/instahome.dart
+++ instagramui_clone/lib/instahome.dart
@@ -20,13 +20,47 @@ class InstaHome extends StatelessWidget {
     return new Scaffold(
       appBar: topBar,
       body: new Container(),
-      // bottomNavigationBar: new Container(
-      //   color: Colors.white,
-      //   height: 50.0,
-      //   child: new BottomAppBar(
-      //     child: ,
-      //   ),
-      // )
+      bottomNavigationBar: new Container(
+        color: Colors.white,
+        height: 50.0,
+        child: new BottomAppBar(
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              new IconButton(
+                icon: Icon(
+                  Icons.home,
+                ),
+                onPressed: () {},
+              ),
+              new IconButton(
+                icon: Icon(
+                  Icons.search,
+                ),
+                onPressed: null,
+              ),
+               new IconButton(
+                icon: Icon(
+                  Icons.add_box,
+                ),
+                onPressed: null,
+              ), 
+               new IconButton(
+                icon: Icon(
+                  Icons.favorite,
+                ),
+                onPressed: null,
+              ),
+               new IconButton(
+                icon: Icon(
+                  Icons.account_box,
+                ),
+                onPressed: null,
+              ),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
